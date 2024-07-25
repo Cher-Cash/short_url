517b0f7f0d42d8a9cef137c051d713e09317d2db
Cher-Cash <kovach.aleksey19@yandex.ru>
2024-07-25 20:01:48 +1000

    refactoring short_url.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
+import short_url
 from flask import Flask, render_template, request, abort, redirect
-from short_url import ShortUrl
+
 
 app = Flask(__name__)
 links = {}
@@ -8,8 +9,7 @@ def index():
     global links
     url = request.args.get('url')
     if url:
-        short_url_ = ShortUrl(url)
-        generated_link = short_url_.generate_hash()
+        generated_link = short_url.generate_hash(url)
         links[generated_link] = url
         return render_template('index.html', generated_link=generated_link)
     return render_template('index.html')
@@ -19,8 +19,6 @@ def short_url_route(short_url_link):
     global links
     long_url = links.get(short_url_link)
     print(links)
-    print(long_url)
-    print(short_url_link)
     if long_url:
         return redirect(long_url)
     return abort(404)
