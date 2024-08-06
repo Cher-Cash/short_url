4e696a1bebc084634c32ddde263753b5c10045ae
Kovach Alexey <kovach.aleksey19@yandex.ru>
2024-08-06 21:58:00 +1000

    flake fixes

--- short_url.py
+++ short_url.py
@@ -1,4 +1,5 @@
 import hashlib
+from datetime import datetime
 
 
 def generate_hash(long_url: str, current_time):
@@ -6,7 +7,7 @@ def generate_hash(long_url: str, current_time):
     hash_object.update(long_url.encode('utf-8'))
     hash_object.update(str(current_time.timestamp()).encode('utf-8'))
     hash_hex = hash_object.hexdigest()
-    ascii_hash = [0] * 16
+    ascii_hash = [''] * 16
     for index_, i in enumerate(range(0, len(hash_hex), 2)):
         ascii_hash[index_] = process_byte(hash_hex[i:i + 2])
     hash_ = ''.join(ascii_hash)
@@ -15,22 +16,21 @@ def generate_hash(long_url: str, current_time):
 
 def process_byte(num_line):
     num = int(num_line, 16)
-    #гарантия что ascii_num будет в пределах 48-105 (диапазон который включает все цифры и многие буквы)
+    # гарантия что ascii_num будет в пределах 48-105 (диапазон, который включает все цифры и многие буквы)
     ascii_num = (num % 58) + 48
-    if (ascii_num >= 58 and ascii_num <= 64) or (ascii_num >= 91 and ascii_num <= 96):
+    if (58 <= ascii_num <= 64) or (91 <= ascii_num <= 96):
         ascii_char = '_'
     else:
         ascii_char = chr(ascii_num)
     return ascii_char
 
 
-
 def main(long_url: str):
-    hash_ = generate_hash(long_url)
+    current_time = datetime.now()
+    hash_ = generate_hash(long_url, current_time)
     print(hash_)
     return hash_
 
+
 if __name__ == "__main__":
     main("https://example.com/some/very/long/url/that/needs/to/be/shortened")
-
-
